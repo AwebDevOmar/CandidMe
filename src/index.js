469172b478e43d6d70cda34816e442ffcdce0781
AwebDevOmar <omar.sk2004@gmail.com>
2023-05-16 11:37:24 +0600

    hashrouted

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import './styles/index.css';
 import './styles/inbox.css'
 import './styles/card.css';
@@ -10,9 +10,9 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-    <Router>
+    <HashRouter>
     <App />
-    </Router>
+    </HashRouter>
 
 );
 
